7f6b105f01ddfafd5ae3a0504b1c0c84d10f9abe
scenaristeur <scenaristeur@gmail.com>
2023-04-11 16:13:57 +0200

    initialisation

---
+++ src/components/MembreEquipe.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="hello">
+         <h3>{{nom}}</h3>
+         <i>age : {{ age }}</i>
+        <img alt="Vue logo" width="50" src="../assets/logo.png">
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'MembreEquipe',
+    props: 
+      ['nom', 'age']
+    
+  }
+  </script>
+  
+  <style scoped>
+  h3 {
+    margin: 40px 0 0;
+    color: #42b983;
+  }
+  </style>
+  
---
+++ src/views/EquipeView.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div class="equipe">
+    <h1>C'est la page de l'équipe</h1>
+
+<MembreEquipe nom="John Wayne" age="25" />
+<MembreEquipe nom = "Bob Marley" age="36"/>
+<MembreEquipe nom="Isi Dor" age="100"/>
+<MembreEquipe nom="Filip Mo "/>
+<MembreEquipe nom="Hey Ho"/>
+<MembreEquipe nom="CElia Too"/>
+
+
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MembreEquipe from '@/components/MembreEquipe.vue'
+
+export default {
+  name: 'EquipeView',
+  components: {
+    MembreEquipe
+  }
+}
+</script>
+
